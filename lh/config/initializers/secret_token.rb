e0813f0984fafffe8cf9c5857b4db79d610e185c
U-LUXRESEARCHINC\chris.greene <chris.greene@lux-bos-e6320-6.luxresearchinc.com>
2011-09-19 10:11:00 -0400

    Initial lh commit

---
+++ lh/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lh::Application.config.secret_token = 'b85463071cc96320dcfb1749761d80191e19ddc4f0e55847e097a1c5fb89bb495e338e52f50e2569e0d1cc20aadabeeef5b79e4ee7ec4829406b2d8dfd8c7842'
